80aa1b7c3e94d67b5f06665f63210044c6582f0f
gaoxinwei0527 <gaoxinwei0527@gmail.com>
2014-10-15 16:56:08 -0500

    Create Solution_1.java

---
+++ LeetCode/Freq5/Valid-Number/Solution_1.java
@@ -0,0 +1,15 @@
+/*Regex*/
+
+public class Solution_1 {
+    public boolean isNumber(String s) {  
+        if(s.trim().isEmpty()){  
+            return false;  
+        }  
+        String regex = "[-+]?(\\d+\\.?|\\.\\d+)\\d*(e[-+]?\\d+)?";  
+        if(s.trim().matches(regex)){  
+            return true;  
+        }else{  
+            return false;  
+        }
+    }
+}
