9091b984af441daad8ebe28158642a73f6a8e9bc
gaoxinwei0527 <gaoxinwei0527@yahoo-inc.com>
2017-12-11 09:56:06 -0800

    leetcode 104-106

--- LeetCode/L_Leetcode_104.java
+++ LeetCode/L_Leetcode_104.java
@@ -8,4 +8,20 @@ package LeetCode;
  The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
  */
 public class L_Leetcode_104 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    /**
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
 }
+
--- LeetCode/L_Leetcode_105.java
+++ LeetCode/L_Leetcode_105.java
@@ -9,4 +9,31 @@ package LeetCode;
  You may assume that duplicates do not exist in the tree.
  */
 public class L_Leetcode_105 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    /**
+     * @param preorder
+     * @param inorder
+     * @return
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        return build(preorder, 0, n - 1, inorder, 0, n - 1);
+    }
+
+    private TreeNode build(int[] preorder, int i, int j, int[] inorder, int a, int b){
+        if(i > j) return null;
+        TreeNode root = new TreeNode(preorder[i]);
+        if(i == j) return root;
+        int k = a;
+        while(inorder[k] != preorder[i]) k++;
+        root.left = build(preorder, i + 1, k - a + i, inorder, a, k - 1);
+        root.right = build(preorder, k - a + i + 1, j, inorder, k + 1, b);
+        return root;
+    }
 }
