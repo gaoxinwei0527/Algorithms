3a259c20fe068c0cd9f4821dba88298143161216
gaoxinwei0527 <gaoxinwei0527@yahoo-inc.com>
2017-12-11 21:02:25 -0800

    leetcode 107-108

--- LeetCode/L_Leetcode_108.java
+++ LeetCode/L_Leetcode_108.java
@@ -21,5 +21,28 @@ package LeetCode;
  -10  5
  */
 public class L_Leetcode_108 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
 
+    /**
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode build(int[] nums, int i, int j){
+        if(i > j) return null;
+        if(i == j) return new TreeNode(nums[i]);
+        int m = i + (j - i) / 2;
+        TreeNode root = new TreeNode(nums[m]);
+        root.left = build(nums, i, m - 1);
+        root.right = build(nums, m + 1, j);
+        return root;
+    }
 }
