23049a0946aee1d5a47a8849c8176ce08cf50a0f
gaoxinwei0527 <gaoxinwei0527@yahoo-inc.com>
2017-12-13 23:49:43 -0800

    read leetcode 121-137

---
+++ LeetCode/L_Leetcode_131.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+/**
+ 131. Palindrome Partitioning
+
+ Given a string s, partition s such that every substring of the partition is a palindrome.
+
+ Return all possible palindrome partitioning of s.
+
+ For example, given s = "aab",
+ Return
+
+ [
+ ["aa","b"],
+ ["a","a","b"]
+ ]
+ */
+public class L_Leetcode_131 {
+}
