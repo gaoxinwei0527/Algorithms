21f48e918cb46f24eab38895cb3e1e47167b5746
gaoxinwei0527 <gaoxinwei0527@yahoo-inc.com>
2017-12-24 14:12:50 -0800

    leetcode 156-159

--- LeetCode/H_Leetcode_156.java
+++ LeetCode/H_Leetcode_156.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.Stack;
+
 /**
  156. Binary Tree Upside Down
 
@@ -21,4 +23,47 @@ package LeetCode;
  confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
  */
 public class H_Leetcode_156 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    /**
+     * @param root
+     * @return
+     *
+     * firstly the leftmost node must be the new root. so we can use stack to push in left children until reach the leftmost one.
+     * then in stack from bottom to top, it would be all parent to left child pairs.
+     * For each popped left child, set its left child to parent's right child, set its right child to its parent. That's it.
+     *
+     * Because right child would have at most single node. so no recursive on right subtree.
+     *
+     * Time - O(n)
+     * Space - O(n), worst case there is no right subtrees.
+     */
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        Stack<TreeNode> st = new Stack<>();
+        while(root != null){
+            st.push(root);
+            root = root.left;
+        }
+
+        if(st.isEmpty()) return null;
+        TreeNode newRoot = st.peek();
+
+        while(!st.isEmpty()){
+            TreeNode next = st.pop();
+            if(!st.isEmpty()){
+                next.left = st.peek().right;
+                next.right = st.peek();
+            }else{
+                next.left = null;
+                next.right = null;
+            }
+        }
+
+        return newRoot;
+    }
 }
--- LeetCode/L_Leetcode_159.java
+++ LeetCode/L_Leetcode_159.java
@@ -1,5 +1,8 @@
 package LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  159. Longest Substring with At Most Two Distinct Characters
 
@@ -10,4 +13,42 @@ package LeetCode;
  T is "ece" which its length is 3.
  */
 public class L_Leetcode_159 {
+    /**
+     * @param s
+     * @return
+     *
+     * sliding window, make the window maintain a substring with at most 2 distinct characters.
+     * init state - get max j that [0, j) window contains at most 2 distinct characters, then we get max initialized as j
+     * iterate - move j -> j + 1, if break window condition, keep moving i until it's satisfied again. Update max with j - i
+     *
+     * Time - O(n)
+     * Space -O(n), can be optimized
+     */
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        Map<Character, Integer> window = new HashMap<>();
+        char[] arr = s.toCharArray();
+        int i = 0;
+        int j = 0;
+
+        while(j < arr.length && (window.size() < 2 || window.containsKey(arr[j]))){
+            char c = arr[j++];
+            window.put(c, window.getOrDefault(c, 0) + 1);
+        }
+        int max = j;
+
+        while(j < arr.length){
+            char c = arr[j++];
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            while(window.size() > 2){
+                char a = arr[i++];
+                window.put(a, window.get(a) - 1);
+                if(window.get(a) == 0) window.remove(a);
+            }
+
+            max = Math.max(max, j - i);
+        }
+
+        return max;
+    }
 }
