70a71de560492ec8b9d4d3bf254557cc23210586
gaoxinwei0527 <gaoxinwei0527@yahoo-inc.com>
2018-01-06 19:48:07 -0800

    leetcode 237 450

--- LeetCode/H_Leetcode_450.java
+++ LeetCode/H_Leetcode_450.java
@@ -41,4 +41,51 @@ package LeetCode;
  4   7
  */
 public class H_Leetcode_450 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    /**
+     * @param root
+     * @param key
+     * @return
+     *
+     * since it's not required to make the result tree balanced. so after deletion, we can do-
+     * 1. if cur has right child, connect pre to cur.right, connect left most node in right subtree of cur to left subtree of cur
+     * 2. if cur doesn't have right child, connect pre to cur.left
+     */
+    public TreeNode deleteNode(TreeNode root, int key) {
+        TreeNode dummy = new TreeNode(0);
+        dummy.right = root;
+
+        TreeNode pre = dummy;
+        TreeNode cur = root;
+        while(cur != null && cur.val != key){
+            pre = cur;
+            if(cur.val < key) cur = cur.right;
+            else cur = cur.left;
+        }
+
+        if(cur == null) return root;
+
+        if(cur.right == null){
+            if(pre.right == cur) pre.right = cur.left;
+            else pre.left = cur.left;
+        }else{
+            if(pre.right == cur) pre.right = cur.right;
+            else pre.left = cur.right;
+
+            TreeNode tmp = cur.right;
+            while(tmp.left != null) tmp = tmp.left;
+            tmp.left = cur.left;
+        }
+
+        cur.left = null;
+        cur.right = null;
+
+        return dummy.right;
+    }
 }
