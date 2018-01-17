00527d5ca483a67b1797d9130484de69da7ef4dd
gaoxinwei0527 <gaoxinwei0527@yahoo-inc.com>
2018-01-16 22:59:50 -0800

    leetcode 282

--- LeetCode/H_Leetcode_282.java
+++ LeetCode/H_Leetcode_282.java
@@ -1,5 +1,8 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  282. Expression Add Operators
 
@@ -15,4 +18,51 @@ package LeetCode;
  Special thanks to @davidtan1890 for adding this problem and creating all test cases.
  */
 public class H_Leetcode_282 {
+    /**
+     * @param num
+     * @param target
+     * @return
+     *
+     * dfs, the trick is, when we try to * some num, we need to minus previous added num, multiply the num with current processing num, and add the result back.
+     * because '*' has higher priority than '+' & '-', it will happen first.
+     */
+    public List<String> addOperators(String num, int target) {
+        List<String> res = new ArrayList<>();
+        if(num.length() == 0) return res;
+        helper(res, num, "", 0, 0, target, 0);
+        return res;
+    }
+
+    private void helper(List<String> res, String num, String build, int cur, long tmp, long target, long prev){
+        if(cur == num.length()){
+            if(tmp == target){
+                res.add(build);
+            }
+            return;
+        }
+
+        if(cur == 0){
+            if(num.charAt(cur) == '0'){
+                helper(res, num, "0", cur + 1, 0, target, 0);
+            }else{
+                for(int i = cur + 1; i <= num.length(); i++){
+                    long next = Long.parseLong(num.substring(cur, i));
+                    helper(res, num, num.substring(cur, i), i, next, target, next);
+                }
+            }
+        }else{
+            if(num.charAt(cur) == '0'){
+                helper(res, num, build + "+0", cur + 1, tmp, target, 0);
+                helper(res, num, build + "-0", cur + 1, tmp, target, 0);
+                helper(res, num, build + "*0", cur + 1, tmp - prev, target, 0);
+            }else{
+                for(int i = cur + 1; i <= num.length(); i++){
+                    long next = Long.parseLong(num.substring(cur, i));
+                    helper(res, num, build + "+" + next, i, tmp + next, target, next);
+                    helper(res, num, build + "-" + next, i, tmp - next, target, -next);
+                    helper(res, num, build + "*" + next, i, tmp - prev + (prev * next), target, prev * next);
+                }
+            }
+        }
+    }
 }
